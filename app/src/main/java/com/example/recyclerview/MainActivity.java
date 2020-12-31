6ce1f663dabd12afe6216d68a43530820b70f957
fatihahmrs <fatihahrosly99@gmail.com>
2020-12-31 20:06:02 +0800

    this is a recyclerview tutorial

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.recyclerview;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -70,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             notifyDataSetChanged();
         }
 
-        class MyViewHolder extends RecyclerView.ViewHolder {
+        class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
             public TextView textView;
             public TextView textView2;
             public ImageView imageView;
@@ -79,6 +81,17 @@ public class MainActivity extends AppCompatActivity {
                 textView = itemView.findViewById(R.id.versiontitle);
                 textView2 = itemView.findViewById(R.id.versionnumber);
                 imageView = itemView.findViewById(R.id.icon);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                String name = elements.get(getAdapterPosition()).getName();
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+
+
             }
         }
     }
